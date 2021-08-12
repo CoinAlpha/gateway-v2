9c4851939e70cdac456471cb2e60a82c8674414a
james-hummingbot <james@hummingbot.io>
2021-08-12 15:46:05 +0200

    Begin Ethereum test script

--- manual-tests/curl-with-private-key.sh
+++ manual-tests/curl-with-private-key.sh
@@ -6,5 +6,7 @@
 # test that the gateway-api server is running
 curl -X GET -k --key $GATEWAY_KEY --cert $GATEWAY_CERT https://localhost:5000/
 
+curl -X GET -k --key $GATEWAY_KEY --cert $GATEWAY_CERT https://localhost:5000/eth
+
 # get balances for your private key
 curl -X POST -k --key $GATEWAY_KEY --cert $GATEWAY_CERT -H "Content-Type: application/json" -d "{\"privateKey\":\"$ETH_PRIVATE_KEY\",\"tokenSymbols\":[\"ETH\",\"WETH\",\"DAI\"]}" https://localhost:5000/eth/balances
